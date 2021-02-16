e5d01f9beaca70f4f41e13330aa7121ffff34f9c
Rozi Vector <rozi.qood16@gmail.com>
2021-02-16 16:49:32 +0700

    Create calculator.py

---
+++ pertemuan1/calculator.py
@@ -0,0 +1,5 @@
+print(17/3) # pecahan dengan hasil bilangan desimal
+print(17//3) # hasil pecahan yang disederhanakan dengan bilangan bulat terbesar
+print(17%3) # modulo
+print(5*3+2) # hasil * penbagian + modulo = bilangan bulat pertama
+print(5**2) # hasil pangkat
